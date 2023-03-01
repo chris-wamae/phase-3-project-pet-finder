74021162128920f47769ae3a25a574220612bb71
chris-wamae <chriswamae123@gmail.com>
2023-03-01 16:58:38 +0300

    Created zustand store and tested it with dummy API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './views/Home';
+import PetCard from './components/Petcard';
+
 
 function App() {
   return (
     <>
+    <Home/>
+    <PetCard/>
     </>
   );
 }
